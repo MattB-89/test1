5d02b87167adf60e02c59655d39fd7b50df4f60e
Matt B <mattbaars@me.com>
2025-06-17 17:27:00 -0500

    updated app with multiplication

--- helloWorld1/src/helloWorld1/HelloWorld1.java
+++ helloWorld1/src/helloWorld1/HelloWorld1.java
@@ -5,7 +5,11 @@ public class HelloWorld1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
 		System.out.println("Hello world!!  Again.");
-		System.out.println("Hello yet again!");
+		System.out.println("Hello yet again!\n");
+		
+		int result = 25 * 3;
+		//System.out.println("\n");
+		System.out.println(result);
 	}
 
 }
